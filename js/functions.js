23f9f8dd87ebeaaed5fd81f6343d41eaeff621c2
Воронина Ксения <kseniavoronina1211@gmail.com>
2025-10-20 10:26:15 +0500

    5.8. Модуляция

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,22 @@
+
 function checkMeetingAvailability(startWork, endWork, startMeeting, meetingDuration) {
 
   function timeToMinutes(time){
-    const [hours, minutes] = time.split(':').map(Number)
-    return hours * 60 + minutes
-  }
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
+  };
 
-  startWork = timeToMinutes(startWork)
-  endWork = timeToMinutes(endWork)
+  startWork = timeToMinutes(startWork);
+  endWork = timeToMinutes(endWork);
 
-  startMeeting = timeToMinutes(startMeeting)
-  const endMeeting = startMeeting + meetingDuration
+  startMeeting = timeToMinutes(startMeeting);
+  const endMeeting = startMeeting + meetingDuration;
 
-  //console.log(endMeeting <= endWork && startMeeting >= startWork)"
+  //console.log(endMeeting <= endWork && startMeeting >= startWork);
 
   return endMeeting <= endWork && startMeeting >= startWork
 
-}
+};
 
 checkMeetingAvailability('08:00', '17:30', '14:00', 90); // true
 checkMeetingAvailability('8:0', '10:0', '8:0', 120);     // true
