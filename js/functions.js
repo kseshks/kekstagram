96e02ab358c45caec1774fa0f89888a247e4a42c
Воронина Ксения <kseniavoronina1211@gmail.com>
2025-10-20 15:35:25 +0500

    5.9. Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,9 @@
 function checkMeetingAvailability(startWork, endWork, startMeeting, meetingDuration) {
 
-  function timeToMinutes(time){
+  function timeToMinutes(time) {
     const [hours, minutes] = time.split(':').map(Number);
     return hours * 60 + minutes;
-  };
+  }
 
   startWork = timeToMinutes(startWork);
   endWork = timeToMinutes(endWork);
@@ -12,13 +12,10 @@ function checkMeetingAvailability(startWork, endWork, startMeeting, meetingDurat
   const endMeeting = startMeeting + meetingDuration;
 
   return endMeeting <= endWork && startMeeting >= startWork;
-};
+}
 
 checkMeetingAvailability('08:00', '17:30', '14:00', 90); // true
 checkMeetingAvailability('8:0', '10:0', '8:0', 120);     // true
 checkMeetingAvailability('08:00', '14:30', '14:00', 90); // false
 checkMeetingAvailability('14:00', '17:30', '08:0', 90);  // false
 checkMeetingAvailability('8:00', '17:30', '08:00', 900); // false
-
-
-
