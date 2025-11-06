52f4603ecd882e3793feb064333ed69a9b1bca34
Воронина Ксения <kseniavoronina1211@gmail.com>
2025-11-07 01:01:10 +0500

    7.11. Отрисуй меня полностью(исправление ошибок)

--- js/render-pictures.js
+++ js/render-pictures.js
@@ -1,24 +1,26 @@
 const pictures = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const renderPicture = (picture)=>{
-  const {url, description, comments, likes} = picture;
+const renderPicture = (picture) => {
+  const { url, description, comments, likes } = picture;
   const pictureElement = pictureTemplate.cloneNode(true);
 
-  pictureElement.querySelector('.picture_img').src = url;
-  pictureElement.querySelector('.picture_img').alt = description;
-  pictureElement.querySelector('.picture_comments').textContent = comments.length;
-  pictureElement.querySelector('.picture_likes').textContent = likes;
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__img').alt = description;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
 
   return pictureElement;
 };
 
-const renderPictures = (objects)=>{
+const renderPictures = (objects) => {
+  pictures.querySelectorAll('.picture').forEach(picture => picture.remove());
+
   const fragment = document.createDocumentFragment();
-  for (let i = 0; i < objects.length; i++){
+  for (let i = 0; i < objects.length; i++) {
     fragment.appendChild(renderPicture(objects[i]));
   }
   pictures.appendChild(fragment);
 };
 
-export {renderPictures};
+export { renderPictures };
